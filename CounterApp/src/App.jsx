05d1c98422429c1030a89105249ee531158af69a
이연서 <dlth010@naver.com>
2024-12-24 20:56:53 +0900

    add 'Even' component

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Viewer from "./components/viewer";
 import Controller from "./components/controller";
+import Even from "./components/Even";
 import { useState } from "react";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <h1>Simple Counter</h1>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />

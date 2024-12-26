2dff4fb33fe4790c1367bda35b889df0f39126bc
이연서 <dlth010@naver.com>
2024-12-26 18:54:27 +0900

    feat: add reusable common components for the project

--- EmotionDiary/src/App.jsx
+++ EmotionDiary/src/App.jsx
@@ -1,9 +1,52 @@
 import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Button from "./components/Button";
+
+import { getEmotionImage } from "./util/get-emotion-image";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
+import Header from "./components/Header";
 
 function App() {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
   return (
     <>
-      <div>App</div>
+      <Header
+        leftChild={<Button text={"Left"} />}
+        title="Header"
+        rightChild={<Button text={"right"} />}
+      />
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭!");
+        }}
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
     </>
   );
 }
